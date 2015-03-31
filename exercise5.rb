1696d25e0ac8bc30546ef3de055a9e5d39d0d1f4
Dan Comand <dcomand@gmail.com>
2015-03-31 17:28:26 -0400

    Added first elsif statementto output 'Maker' if the number is a multiple of 5.

---
+++ exercise5.rb
@@ -0,0 +1,16 @@
+counter = (1..100)
+
+counter.each do | num |
+	if num % 3 == 0 && num % 5 == 0
+		puts "Bitmaker"
+	elsif num % 5 == 0
+		puts "Maker"
+	elsif num % 3 == 0
+		puts "Bit"
+	else 
+		puts num	
+	end
+end
+
+
+
